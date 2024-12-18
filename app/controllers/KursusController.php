95f73f7e8697fb067e2fe769185eb8666e4e8f31
WindyAnggitaPutri <windyanggitaputri@gmail.com>
2024-12-18 09:36:11 +0700

    2

--- app/controllers/KursusController.php
+++ app/controllers/KursusController.php
@@ -16,8 +16,31 @@ class KursusController {
     }
 
     public function create() {
+        $users = $this->kursusModel->getAllUser();
+        $materi = $this->kursusModel->getAllMateri();
+        $kursus = $this->kursusModel->getAllKursus();
         require_once '../app/views/user/create.php';
+
     }
+
+//     public function deskripsi(){
+// $id_user = $_GET['id_user']; // Atau menggunakan $_POST atau parameter lain
+// $deskripsi = isset($kursus['deskripsi']) ? $kursus['deskripsi'] : ''; // Fungsi untuk mengambil deskripsi
+// $this->view('user/edit', ['deskripsi' => $deskripsi]); 
+
+//     }
+
+public function deskripsi(){
+ // Pastikan di controller
+$deskripsi = $this->kursusModel->getdeskripsiById($deskripsi); // Dapatkan data kursus berdasarkan id_kursus
+// Kemudian kirim ke view
+$this->view('user/edit', $kursus); // Pastikan kursus berisi deskripsi yang benar
+
+
+
+}
+
+    
 
 
     public function store() {
@@ -33,19 +56,24 @@ class KursusController {
     }
     // Show the edit form with the user data
     public function edit($id_kursus) {
-        $kursus = $this->kursusModel->find($id_kursus); // Assume find() gets user by ID
+        $kursus = $this->kursusModel->find($id_kursus); 
+        $users = $this->kursusModel->getAllUser();
+        $materi = $this->kursusModel->getAllMateri();
+        $kursus = $this->kursusModel->getAllKursus();
         require_once __DIR__ . '/../views/user/edit.php';
     }
 
     // Process the update request
-    public function update($id_kursus, $data) {
-        $updated = $this->kursusModel->update($id_kursus, $data);
+    public function update($id_kursus, $id_user, $id_materi, $judul_kursus, $instruktur, $deskripsi, $durasi) {
+        $updated = $this->kursusModel->update($id_kursus, $id_user, $id_materi, $judul_kursus, $instruktur, $deskripsi, $durasi);
         if ($updated) {
             header("Location: /user/index"); // Redirect to user list
         } else {
             echo "Failed to update kursus.";
         }
     }
+
+    // $updated = $this->kursusModel->update($id_kursus, $id_user, $id_materi, $judul_materi, $instruktur, $deskripsi, $durasi);
 
     // Process delete request
     public function delete($id_kursus) {
